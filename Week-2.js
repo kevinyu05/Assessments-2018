c7ce63daa277cb508c72e7c4acd38b8418b659a9
kevinyu05 <44944091+kevinyu05@users.noreply.github.com>
2018-12-17 20:54:57 -0800

    Update Week-2.js

--- Week-2.js
+++ Week-2.js
@@ -1,44 +1,55 @@
 // 1. Write an anonymous function that takes one argument of type array and returns the contents of that array in reverse order.
 
-// test array: var testCase = ["one", "two", "three", "four"]
+var testCase = ["one", "two", "three", "four"]
 
-var reverseArray(list) {
+var reverseArray = function(list) {
   // ... your code here
+  return list.reverse()
 }
 
-reverseArray(test)
+console.log(reverseArray(testCase))
 
 // expected output: ["four", "three", "two", "one"]
 
 // 2. Given the object below, fill in how to find specific information:
 
 var bicycle = {
-	type: "Roadbike",
-	gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
-	wheels: {
-		count: 2,
-		specs: ["road tires", "AX-7563", "80-115 PSI"],
-		brand: "Trek"
-	}
+  type: "Roadbike",
+  gear: ["comfy seat", "cool handlebars", "vintage bell", "toe clips"],
+  wheels: {
+    count: 2,
+    specs: ["road tires", "AX-7563", "80-115 PSI"],
+    brand: "Trek"
+  }
 }
 
 // Log the type of bicycle:
-console.log()
+console.log(bicycle.type)
 
 // Log just the bell from the list of gear
-console.log()
+console.log(bicycle.gear[2])
 
 // Log the correct PSI for the tires
-console.log()
+console.log(bicycle.wheels.specs[2])
 
 // 3. Write a function that takes two arguments, a string and an individual letter. The function should return a count of how many times the letter appears in the string.
+function charCount(str, letter) {
+  var letterCount = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str.charAt(i) == letter) {
+      letterCount += 1;
+    }
+  }
+  return letterCount;
+}
 
 // examples:
 
-countLetter("Hello World", "l")
+console.log(charCount("Hello World", "l"))
+//
+// // should return => 3
+//
+console.log(charCount("Hello World", "z"))
+//
+// // should return => 0
 
-// should return => 3
-
-countLetter("Hello World", "z")
-
-// should return => 0
